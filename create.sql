093f2f635dcb35b66f552a6d8c0bbb17f4d00889
espinel10 <espinel_11@hotmail.com>
2020-09-27 20:08:26 -0500

    model

---
+++ create.sql
@@ -0,0 +1,4 @@
+create table persona (  id INT(3) NOT NULL AUTO_INCREMENT,  name varchar(155),  apellidos varchar(155),  CONSTRAINT id_personas_pk PRIMARY KEY (id)    );
+INSERT INTO TABLE persona (name,apellidos) VALUES ("Cristiano","Ronaldo"), ("Messi","notevayas:(");
+
+
